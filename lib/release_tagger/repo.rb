780b19aab48dbfd930b9dda75cc618bb950db75b
Nadir Lloret <nadir.lloret@gmail.com>
2015-10-29 15:09:16 +0000

    Take into account different architectures when querying the repository

--- lib/release_tagger/repo.rb
+++ lib/release_tagger/repo.rb
@@ -7,7 +7,8 @@ module ReleaseTagger
     PACKAGECLOUD_ACCOUNT = 'lostmyname'
     REPO = 'qa'
     ITEMS_PER_PAGE = 1000
-    VERSIONS_URL = "https://packagecloud.io/api/v1/repos/#{PACKAGECLOUD_ACCOUNT}/#{REPO}/package/rpm/el/7/%s/x86_64/versions.json?per_page=#{ITEMS_PER_PAGE}"
+    ARCHS = %w(noarch x86_64)
+    VERSIONS_URL = "https://packagecloud.io/api/v1/repos/#{PACKAGECLOUD_ACCOUNT}/#{REPO}/package/rpm/el/7/%s/%s/versions.json?per_page=#{ITEMS_PER_PAGE}"
 
     def get_api_token
       home_config_file = Pathname.new(File.join(File.expand_path('~'), '.release_tagger', 'packagecloud_token'))
@@ -32,13 +33,13 @@ Consider setting your packagecloud api token in any of:
       package_cloud_api_token
     end
 
-    def get_max_package_version(package_name)
+    def get_json_response(url)
       pc_api_token = get_api_token
-      uri = URI.parse(VERSIONS_URL % package_name)
+      uri = URI.parse(url)
       req = Net::HTTP::Get.new(uri)
       req.basic_auth pc_api_token, ''
       response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') {|http| http.request(req)}
-
+      puts url
       unless response.is_a?(Net::HTTPOK)
         puts "Error trying to get latest version of package #{package_name}"
         puts response.code
@@ -46,10 +47,20 @@ Consider setting your packagecloud api token in any of:
       end
 
       res = JSON.parse(response.body)
-      release_version = '1.0.0'
-      unless res == []
-        latest_package = res.select { |package| package['name'] == package_name}.max_by{ |package| package['version']}
-        release_version = latest_package['version']
+      res
+
+    end
+
+    def get_max_package_version(package_name)
+      release_version = '0.0.0'
+      ARCHS.each do |arch|
+        res = get_json_response(VERSIONS_URL % [package_name, arch])
+        unless res == []
+          latest_package = res.select { |package| package['name'] == package_name}.max_by{ |package| package['version']}
+          if Gem::Version.new(latest_package['version']) > Gem::Version.new(release_version)
+            release_version = latest_package['version']
+          end
+        end
       end
       release_version
     end
